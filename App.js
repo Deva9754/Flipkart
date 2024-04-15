27fb6b65140d3b9554c77dd15ce56457febf6cb2
Deva9754 <deva.sahu2001@gmail.com>
2024-04-16 00:42:48 +0530

    Initial commit of project

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+  "div",
+  { id: "headdd" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "im a child"),
+    React.createElement("h2", {}, "im a child"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// const heading1 = document.createElement("h1");
+// heading1.innerHTML = "Hello from java S";
+
+// const root1 = document.getElementById("root");
+// root1.append(heading1);
