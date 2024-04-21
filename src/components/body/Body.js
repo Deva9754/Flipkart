60b06a8f7a32ac001aeddd0bfbdc4787317b5a12
Deva9754 <deva.sahu2001@gmail.com>
2024-04-21 20:28:20 +0530

    " list item added"

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,20 +3,22 @@ import "./Body.css";
 import ImageSlider from "./carousel/ImageSlider";
 import images from "../../utils/Image";
 import NavItems from "./navbar/NavItems";
-import { Container } from "@mui/material";
+import { Container, ListItem } from "@mui/material";
 import ItemContainer from "./itemContainer/ItemContainer";
 // import ShimmerContainer from "./shimmer/ShimmerContainer";
+import ListItem from "./ListItem/ListItem";
 
 const Body = () => {
   return (
     <div className="Body">
-      <NavItems />
+      {/* <NavItems />
       <ImageSlider images={images} />
       <div>
         Best of Products
         <ItemContainer />
-        {/* <ShimmerContainer /> */}
-      </div>
+        {/* <ShimmerContainer /> 
+      </div> */}
+      <ListItem />
     </div>
   );
 };
---
+++ src/components/body/ListItem/ListItem.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import "./ListItem.css";
+
+const ListItem = () => {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      sx={{ width: "50%", maxWidth: 360, bgcolor: "background.paper" }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader
+          component="div"
+          id="nested-list-subheader"
+        ></ListSubheader>
+      }
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText primary="Sent mail" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Drafts" />
+      </ListItemButton>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Inbox" />
+      </ListItemButton>
+    </List>
+  );
+};
+
+export default ListItem;
