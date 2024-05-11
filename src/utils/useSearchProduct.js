09856c39328db172998e0cfc63a526ef6ef7965e
Deva9754 <deva.sahu2001@gmail.com>
2024-05-11 23:57:13 +0530

    "fix:card-issue"

--- src/utils/useSearchProduct.js
+++ src/utils/useSearchProduct.js
@@ -1,18 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const useSearchProduct = (Category) => {
-  const [searchProduct, setSearchProduct] = useState();
+const useSearchProduct = (proTitle) => {
+  const [loading, setLoading] = useState(true);
+  const [searchProduct, setSearchProduct] = useState([]);
 
   const searchApi = async () => {
-    const data = await fetch(
-      `https://dummyjson.com/products/search?q=${Category}`
-    );
-    const response = await data.json();
-    setSearchProduct(response);
+    try {
+      const data = await fetch(
+        `https://dummyjson.com/products/category/${proTitle}`
+      );
+      const response = await data.json();
+      setSearchProduct(response);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
   useEffect(() => {
+    setLoading(true);
     searchApi();
-  }, []);
-  return searchProduct;
+  }, [proTitle]);
+
+  return { loading, searchProduct };
 };
 export default useSearchProduct;
