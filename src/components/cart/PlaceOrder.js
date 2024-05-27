bcf29dbaf4cd6a283352781b2a70da994ff3aa7a
Deva9754 <deva.sahu2001@gmail.com>
2024-05-27 23:12:31 +0530

    placeorder-delay-fixed

--- src/components/cart/PlaceOrder.js
+++ src/components/cart/PlaceOrder.js
@@ -3,7 +3,6 @@ import "./Cart.css";
 const Placeorder = () => {
   return (
     <div className="placeorder-img">
-      {" "}
       <img
         src="https://cdn.dribbble.com/users/1569943/screenshots/11585644/media/966b3325bad0b3fa0f94f5fb992f7ac2.gif"
         alt="place-order-img"
